1c2cc907b71cd5fa6a0c75374c3f9f97f32842a5
solar01 <maksimu964@gmail.com>
2026-09-24 19:40:52 +0300

    Update models.py

--- nature_index/models.py
+++ nature_index/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Biome(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=20_000)
+    image = models.URLField(max_length=500)
 
     related_biomes = models.ManyToManyField(
         'self',
@@ -16,6 +17,7 @@ class Biome(models.Model):
 class Creature(models.Model):
     name = models.CharField(max_length=500)
     description = models.TextField(max_length=20_000)
+    image = models.URLField(max_length=500)
 
     biomes = models.ManyToManyField(Biome, related_name='creatures')
     created_at = models.DateTimeField(auto_now_add=True)
