66b3a338b21f48b106ddcb85513661c29b6dc230
solar01 <maksimu964@gmail.com>
2026-09-25 18:17:16 +0300

    Update views.py

--- config/views.py
+++ config/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def main_view(request):
     return render(request, 'main.html')
+
+def about_view(request):
+    return render(request, 'about.html')
