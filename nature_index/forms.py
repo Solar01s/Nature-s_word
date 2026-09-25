5dddb250c42ce2e2a3bd8647481dacb54b2f2da8
solar01 <maksimu964@gmail.com>
2026-09-25 17:31:21 +0300

    Update forms.py

--- nature_index/forms.py
+++ nature_index/forms.py
@@ -1,39 +1,50 @@
-from django import forms
+from django.shortcuts import render, redirect
 from .models import Biome, Creature
+from django.shortcuts import get_object_or_404
+from .forms import BiomeForm, CreatureForm
+import random
 
-class BiomeForm(forms.ModelForm):
+def index_view(request):
+    if request.method == 'POST':
+        query = request.POST.get('query', '')
+        biomes = Biome.objects.filter(name__icontains=query).prefetch_related('related_biomes', 'creatures')
+        creatures = Creature.objects.filter(name__icontains=query).prefetch_related('biomes', 'related_creatures')
+    else:
+        biomes = Biome.objects.all().prefetch_related('related_biomes', 'creatures')
+        creatures = Creature.objects.all().prefetch_related('biomes', 'related_creatures')
+    mixed = list(biomes) + list(creatures)
+    random.shuffle(mixed)
+    return render(request, 'nature_index/index.html', {'items': mixed})
 
-    creatures = forms.ModelMultipleChoiceField(
-        queryset=Creature.objects.all(),
-        required=False,)
+def new_creature_view(request):
+    if request.method == 'POST':
+        form = CreatureForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('nature_index')
+        else:
+            print("ОШИБКИ СОЗДАНИЯ ФОРМЫ СОЗДАНИЯ: ", form.errors)
+    else:
+        form = CreatureForm()
+    return render(request, 'nature_index/new_creature.html', {'form': form})
 
-    class Meta:
-        model = Biome
-        fields = ['name', 'description', 'image', 'creatures', 'related_biomes']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g. Ocean'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Describe this Biome...'}),
-            'image': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'image url e.g. https"//biome_photos.com/1234/'}),
-            'related_biomes': forms.CheckboxSelectMultiple(),
-            'creatures': forms.CheckboxSelectMultiple(),
-        }
 
-    def save(self, commit=True):
-        instance = super().save(commit=commit)
-        if commit:
-            instance.creatures.set(self.cleaned_data['creatures'])
-        return instance
+def new_biome_view(request):
+    if request.method == 'POST':
+        form = BiomeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('nature_index')
+        else:
+            print("ОШИБКИ СОЗДАНИЯ ФОРМЫ БИОМА: ", form.errors)
+    else:
+        form = BiomeForm()
+    return render(request, 'nature_index/new_biome.html', {'form': form})
 
-class CreatureForm(forms.ModelForm):
-    class Meta:
-        model = Creature
-        fields = ['name', 'description', 'image', 'biomes', 'related_creatures']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g. Tiger'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Describe this Creature...'}),
-            'image': forms.URLInput(
-                attrs={'class': 'form-control', 'placeholder': 'image url e.g. https"//creature_photos.com/1234/'}),
-            'biomes': forms.CheckboxSelectMultiple(),
-            'related_creatures': forms.CheckboxSelectMultiple(),
+def biome_view(request, biome_id):
+    biome = get_object_or_404(Biome, pk=biome_id)
+    return render(request, 'nature_index/biome.html', {'biome': biome})
 
-        }
+def creature_view(request, creature_id):
+    creature = get_object_or_404(Creature, pk=creature_id)
+    return render(request, 'nature_index/creature.html', {'creature': creature})
